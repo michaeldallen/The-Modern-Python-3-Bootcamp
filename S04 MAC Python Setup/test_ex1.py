d200f98cc43d84798188f96a0f7f2bc65ba200ea
michaeldallen <mallen2016@allenhub.net>
2019-09-30 13:08:43 -0400

    Update test_ex1.py

--- S04 MAC Python Setup/test_ex1.py
+++ S04 MAC Python Setup/test_ex1.py
@@ -3,7 +3,7 @@ from pytest import raises
 from unittest.mock import MagicMock
 
 def test_printYourName(monkeypatch):
-    mock_print = MagicMock(return_value="your name b")
+    mock_print = MagicMock()
     monkeypatch.setattr("builtins.print", mock_print)
     print("your name a")
     mock_print.assert_called_once_with("your name")

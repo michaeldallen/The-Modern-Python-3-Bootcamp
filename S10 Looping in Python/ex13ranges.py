92102bb5e3cc7baa60d539e0b8d13a0ec86d8309
Michael Allen <michael@allenhub.net>
2019-10-07 15:33:35 -0400

    range exercise

---
+++ S10 Looping in Python/ex13ranges.py
@@ -0,0 +1,17 @@
+def oddFromTenToTwentyInclusive():
+    start = 10
+    stop = 20 
+    return [ x for x in range(start, stop + 1) if x % 2 ]
+
+
+def sumOfList(l):
+    r=0
+    for v in l:
+        r += v
+    return r
+
+
+if __name__ == "__main__":
+    print(sumOfList(oddFromTenToTwentyInclusive()))
+
+    
---
+++ S10 Looping in Python/test_ex13-ranges.py
@@ -0,0 +1,19 @@
+import pytest
+
+from ex13ranges import oddFromTenToTwentyInclusive, sumOfList
+
+
+def test_oddFromTenToTwentyInclusive():
+    assert oddFromTenToTwentyInclusive() == [11, 13, 15, 17, 19]
+
+def test_sumOfList():
+    assert sumOfList([1,2,3]) == 6
+    assert sumOfList([11,22,33]) == 66
+
+    
+
+    
+
+    
+    
+

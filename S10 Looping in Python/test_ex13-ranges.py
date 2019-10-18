e0024a7ad1c3fa14e8bf8c85012ce363d4e9ae86
Michael Allen <mallen2016@allenhub.net>
2019-10-18 08:20:08 -0400

    revert trst

--- S10 Looping in Python/test_ex13-ranges.py
+++ S10 Looping in Python/test_ex13-ranges.py
@@ -7,7 +7,7 @@ def test_oddFromTenToTwentyInclusive():
     assert oddFromTenToTwentyInclusive() == [11, 13, 15, 17, 19]
 
 def test_sumOfList():
-    assert sumOfList([1,2,3]) == 62
+    assert sumOfList([1,2,3]) == 6
     assert sumOfList([11,22,33]) == 66
 
     

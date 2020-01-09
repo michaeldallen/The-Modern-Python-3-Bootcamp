540cf81bb3d58818654de5d81ae9c93f57391080
Michael Allen <michael@allenhub.net>
2020-01-09 12:07:43 -0500

    user input unit tests

---
+++ S11 Guessing Game/test_guessing.py
@@ -0,0 +1,57 @@
+import pytest
+from unittest.mock import MagicMock
+
+
+# https://www.oreilly.com/library/view/python-cookbook/0596001673/ch14s08.html
+import sys
+
+
+from guessing import getInputFrom1to10
+
+
+def test_getInputFrom1to10_typeIsInt(monkeypatch):
+    monkeypatch.setattr("builtins.input", MagicMock(return_value = "1"))
+    user_input = getInputFrom1to10(sys._getframe().f_code.co_name)
+    assert user_input == 1
+
+
+def test_getInputFrom1to10_wrongType_string(monkeypatch):
+    with pytest.raises(TypeError) as e:
+        monkeypatch.setattr("builtins.input", MagicMock(return_value = "foo"))
+        user_input = getInputFrom1to10(sys._getframe().f_code.co_name)
+
+
+def test_getInputFrom1to10_wrongType_float(monkeypatch):
+    with pytest.raises(TypeError) as e:
+        monkeypatch.setattr("builtins.input", MagicMock(return_value = "1.1"))
+        user_input = getInputFrom1to10(sys._getframe().f_code.co_name)
+
+
+
+def test_getInputFrom1to10_tooLow(monkeypatch):
+    with pytest.raises(ValueError) as e:
+        monkeypatch.setattr("builtins.input", MagicMock(return_value = "0"))
+        user_input = getInputFrom1to10(sys._getframe().f_code.co_name)
+
+
+def test_getInputFrom1to10_tooHigh(monkeypatch):
+    with pytest.raises(ValueError) as e:
+        monkeypatch.setattr("builtins.input", MagicMock(return_value = "11"))
+        user_input = getInputFrom1to10(sys._getframe().f_code.co_name)
+
+
+def test_getInputFrom1to10_OK(monkeypatch):
+    monkeypatch.setattr("builtins.input", MagicMock(return_value = "1"))
+    user_input = getInputFrom1to10(sys._getframe().f_code.co_name)
+
+
+
+@pytest.mark.skip()
+def test_playAgain():
+    assert(0)
+
+@pytest.mark.skip()
+def test_dontPlayAgain():
+    assert(0)
+
+    

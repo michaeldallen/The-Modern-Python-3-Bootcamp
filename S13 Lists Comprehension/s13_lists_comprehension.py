28460bf8cb0be752821e33bbb1a5fd902ca9273a
Michael Allen <michael@allenhub.net>
2020-02-05 14:11:48 -0500

    S12/ex21 and S12/ex22

--- S13 Lists Comprehension/s13_lists_comprehension.py
+++ S13 Lists Comprehension/s13_lists_comprehension.py
@@ -9,3 +9,14 @@ def firstLetter(l):
 def evens(l):
     return [i for i in l if not i % 2]
 
+
+def intersection(l1, l2):
+    return [i for i in l1 if i in l2]
+
+
+def reversal(l):
+    return [i.lower()[::-1] for i in l]
+
+
+def divBy12(l):
+    return [i for i in l if not i % 12]
--- S13 Lists Comprehension/test_s13_lists_comprehension.py
+++ S13 Lists Comprehension/test_s13_lists_comprehension.py
@@ -1,6 +1,9 @@
 from s13_lists_comprehension import bootstrap
 from s13_lists_comprehension import evens
 from s13_lists_comprehension import firstLetter
+from s13_lists_comprehension import intersection
+from s13_lists_comprehension import reversal
+from s13_lists_comprehension import divBy12
 import pytest
 
 
@@ -20,3 +23,26 @@ def test_ex20_evens():
     expected_gazouta = [2, 4, 6]
     gazouta = evens(gazinta)
     assert gazouta == expected_gazouta
+
+
+def test_ex21_intersection():
+    gazinta1 = list(range(1, 5))
+    gazinta2 = list(range(3, 7))
+    expected_gazouta = list(range(3, 5))
+    gazouta = intersection(gazinta1, gazinta2)
+    assert gazouta == expected_gazouta
+
+
+def test_ex21_reversal():
+    gazinta = ["Elie", "Tim", "Matt"]
+    expected_gazouta = ["eile", "mit", "ttam"]
+    gazouta = reversal(gazinta)
+    assert gazouta == expected_gazouta
+
+
+def test_ex22_divBy12():
+    gazinta = list(range(1, 101))
+    expected_gazouta = [12, 24, 36, 48, 60, 72, 84, 96]
+    gazouta = divBy12(gazinta)
+    assert gazouta == expected_gazouta
+

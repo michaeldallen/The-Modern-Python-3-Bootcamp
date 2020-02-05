37d5547885c1daf13875d26f1ece4e45caa29b33
Michael Allen <michael@allenhub.net>
2020-02-05 11:57:55 -0500

    autopep8 cleanup

--- S13 Lists Comprehension/test_s13_lists_comprehension.py
+++ S13 Lists Comprehension/test_s13_lists_comprehension.py
@@ -1,22 +1,21 @@
+from s13_lists_comprehension import bootstrap
 from s13_lists_comprehension import evens
 from s13_lists_comprehension import firstLetter
 import pytest
 
-from s13_lists_comprehension import bootstrap
 
-
-def test_bootstrap():
+def test_ex20_bootstrap():
     assert bootstrap() == "bootstrap"
 
 
-def test_firstLetter():
+def test_ex20_firstLetter():
     gazinta = ["Elie", "Tim", "Matt"]
     expected_gazouta = ["E", "T", "M"]
     gazouta = firstLetter(gazinta)
     assert gazouta == expected_gazouta
 
 
-def test_evens():
+def test_ex20_evens():
     gazinta = [1, 2, 3, 4, 5, 6]
     expected_gazouta = [2, 4, 6]
     gazouta = evens(gazinta)
